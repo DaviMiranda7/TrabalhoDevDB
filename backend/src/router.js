b856a07813194a983f3e589b6c4d1d98671891d1
Davi Gouêa Miranda <davigmirandapro@gmail.com>
2024-04-03 13:02:07 -0300

    updates2

--- backend/src/router.js
+++ backend/src/router.js
@@ -19,4 +19,8 @@ router.delete('/produtos/:id', produtosController.deleteProduto);
 router.put('/clientes/:id', clienteMiddlewares.validaNome, clienteMiddlewares.validaSobrenome, clienteMiddlewares.validaEmail, clienteMiddlewares.validaIdade, clintesController.updateCliente);
 router.put('/produtos/:id', produtosMiddlewares.validaNome, produtosMiddlewares.validaDescricao, produtosMiddlewares.validaPreco, produtosController.updateProduto);
 
+router.get('/', (req, res) => {
+    res.send('Bem-vindo à pagina principal da API! - Para GET & POST produtos ou clientes: http://localhost:3000/produtos ou http://localhost:3000/clientes - Para PUT & DELETE produtos ou clientes: http://localhost:3000/produtos/{id} ou http://localhost:3000/clientes/{id}');
+});
+
 module.exports = router;
